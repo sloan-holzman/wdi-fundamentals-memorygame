8bcfa29e55faddef3309618324a2eeebbee46679
sloan-holzman <sloan.holzman@gmail.com>
2017-10-04 14:40:29 -0400

    created game llogic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,21 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay[0]);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[1]);
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
+};
+var flipCard = function (cardID) {
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
 	} else {
-		alert("Sorry, try again.");
+		console.log("not two cards");
 	}
-} else {
-	console.log("not two cards");
-}
+};
+
+flipCard(0);
+flipCard(2);
